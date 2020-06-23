d5c3bcc657b804661f8d7023159829826f176de2
Cman <48299779+cmaneman@users.noreply.github.com>
2020-06-23 03:37:10 -0400

    Reuploading of JavaScript File
    -Included all necessary Files

---
+++ JavaScript_Arrays/JavaScript_Arrays/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('Hello Humans This Is An Array Program In JavaScript');
+var JS_ArrayNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var JSArrayChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+for (let i = 0; i < 10; i++) {
+    console.log(JS_ArrayNum[i])
+    console.log(JSArrayChar[i])
+}
